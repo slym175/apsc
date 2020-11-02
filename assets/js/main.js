d4aed7217f0a8184abd6fb8066c22784464d7c7b
thuynano <thuyhu9876@gmail.com>
2020-11-02 18:12:28 +0700

    them duoc nhieu trang phet roi :))

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,4 +36,66 @@ $(function () {
         e.preventDefault();
         $('.search-form').toggleClass('d-none');
     })
+
+    $('i.lv1').click(function(e) {
+        e.preventDefault();
+        $(this).parents('li.nav-item').toggleClass('menu-opened');
+    })
+
+    $('i.lv2').click(function(e) {
+        e.preventDefault();
+        $(this).parents('li').toggleClass('menu-opened-l1');
+    })
+
+    $('i.lv3').click(function(e) {
+        e.preventDefault();
+        $(this).parents('li').toggleClass('menu-opened-l2');
+    })
+
+    var syncedSecondary = true;
+    var sync1 = $('.feature-new-image');
+    var sync2 = $('.feature-new-content');
+
+    sync1.owlCarousel({
+        loop: false,
+        margin: 0,
+        animateOut: 'animate__fadeOut',
+        animateIn: 'animate__fadeIn',
+        items: 1,
+        nav: false,
+        dots: false,
+        mouseDrag: false,
+        touchDrag: false,
+        autoHeight: false,
+        autoHeightClass: 'owl-height',
+        onChanged: function(el) {
+            sync2.trigger('to.owl.carousel', [el.item.index, 100, true]);
+        }
+    })
+
+    sync2.owlCarousel({
+        loop: false,
+        margin: 0,
+        items: 1,
+        nav: true,
+        dots: false,
+        autoHeight: false,
+        autoHeightClass: 'owl-height',
+        navText: ['<i class="icons-next"></i>','<i class="icons-prev"></i>'],
+        onChanged: function(el) {
+            sync1.trigger('to.owl.carousel', [el.item.index, 100, true]);
+        }
+    })
+
+    $('.angle-open').click(function(e) {
+        e.preventDefault();
+        $(this).parents('.left-nav').find('.nav-item').removeClass('opened');
+        $(this).parents('.nav-item').toggleClass('opened');
+    })
+
+    $('.angle-open-lv2').click(function(e) {
+        e.preventDefault();
+        $(this).parents('.sub-nav').find('li').removeClass('opened');
+        $(this).parents('.have-subnav').toggleClass('opened')
+    })
 });
